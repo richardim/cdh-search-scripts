ec1421c49946e1a45a6217c74088deb62015c659
richardim <rim@rim-MBP.local>
2014-09-25 18:17:01 -0400

    unstructured docs

---
+++ pdf-indexer/README.txt
@@ -0,0 +1,25 @@
+* pdf indexer, but the default file is tab delimited.
+
+* CSV morphline command takes separator param.
+
+* This is a self contained test data set with csv delimiter.  The idea is to be able
+to test that you can get SOLR working and indexing the test data set csv delimited, then
+modify from there for what you ACTUALLY need.
+  *upload the sample_07 csv file to /user/cloudera to test
+
+To adapt this for different data:
+* If you want to change the default collection name used, change the COLLECTION variable in:
+go-solr-csv.sh
+solr-csv.conf
+
+* Change your fields in solr-csv.conf
+
+* Change your fields in schema-solr-csv.xml
+
+* Change your in go-solr-csv.sh source folder for the map job, stubbed for hive warehouse
+folder by default
+
+* If you want to remove the index files after a demo or to cleanup, you should run delete docs using solrctl before
+deleting and removing the collection.
+
+* run go-solr-csv.sh clean to get to 'clean' slate
---
+++ pdf-indexer/go-solr-pdf.sh
@@ -0,0 +1,64 @@
+# Adapted from Marty Luries sample
+# sample code not supported
+# source from  example by Mark Brooks
+# adapted to be fully scripted
+# long instead of int for faceted salary search
+
+# Some staging folder on the QSVM
+export PROJECT_HOME=/home/cloudera/pdf-solr
+# export SOLR_HOME=/opt/cloudera/parcels/CDH/lib/solr
+
+# Collection name for SOLR
+COLLECTION=INDEXED-PDF-DATA
+
+# Clean UP
+if [ "$1" == "clean" ]; then
+  rm -rf $PROJECT_HOME
+  solrctl collection --deletedocs $COLLECTION
+  solrctl collection --delete $COLLECTION
+  solrctl instancedir --delete $COLLECTION
+  hadoop fs -rmdir --ignore-fail-on-non-empty /solr/$COLLECTION
+  hadoop fs -rmdir -r /user/cloudera/pdfs
+else
+
+# Clean-up
+rm -rf $PROJECT_HOME
+mkdir $PROJECT_HOME
+
+# Copy reference files for execution
+cp solr-pdf.conf $PROJECT_HOME/
+cp log4j-solr-pdf.properties $PROJECT_HOME/log4j.properties
+
+# Initialize local directory with reference files to upload to solr/zookeeper
+# See http://svn.apache.org/viewvc/lucene/dev/trunk/solr/example/solr/README.txt?view=markup
+solrctl --zk localhost:2181/solr instancedir --generate $PROJECT_HOME/$COLLECTION
+
+# Copy our schema to the ref dir
+cp -f schema-solr-pdf.xml $PROJECT_HOME/$COLLECTION/conf/schema.xml
+
+# Create the instancedirs (configs) and create the collection on a single shard (node of cluster)
+solrctl --zk localhost:2181/solr instancedir --delete $COLLECTION
+solrctl --zk localhost:2181/solr instancedir --create $COLLECTION $PROJECT_HOME/$COLLECTION
+solrctl --zk localhost:2181/solr collection --delete $COLLECTION 
+solrctl --zk localhost:2181/solr collection --create $COLLECTION -s 1
+
+# Create intermediate folders
+hadoop fs -rm -r  /user/cloudera/tmp/solroutdir
+hadoop fs -mkdir -p  /user/cloudera/tmp/solroutdir
+hadoop fs -mkdir -p /user/cloudera/pdfs
+hadoop fs -put /home/cloudera/pdf-indexer/pdfs/* /user/cloudera/pdfs
+
+## Do Indexing - Insert your source directory at end of hadoop jar command
+# Dry-Run to test Morphline
+hadoop jar /opt/cloudera/parcels/CDH/lib/solr/contrib/mr/search-mr-*-job.jar org.apache.solr.hadoop.MapReduceIndexerTool -D 'mapred.child.java.opts=-Xmx500m' --log4j $PROJECT_HOME/log4j.properties --morphline-file $PROJECT_HOME/solr-pdf.conf --output-dir hdfs://localhost:8020/user/cloudera/tmp/solroutdir --verbose --go-live --zk-host localhost:2181/solr --collection $COLLECTION --dry-run hdfs://localhost:8020/user/cloudera/pdfs
+
+# Without Dry-run
+#hadoop jar /opt/cloudera/parcels/CDH/lib/solr/contrib/mr/search-mr-*-job.jar org.apache.solr.hadoop.MapReduceIndexerTool -D 'mapred.child.java.opts=-Xmx500m' --log4j $PROJECT_HOME/log4j.properties --morphline-file $PROJECT_HOME/solr-pdf.conf --output-dir hdfs://localhost:8020/user/cloudera/tmp/solroutdir --verbose --go-live --zk-host localhost:2181/solr --collection $COLLECTION hdfs://localhost:8020/user/cloudera/pdfs
+
+echo "Finished!"
+echo SEARCH TIPS: 
+echo wildcard syntax 'file*' 
+echo proximity '"first managers" ~5'
+echo or condition 'file manage*'
+
+fi
